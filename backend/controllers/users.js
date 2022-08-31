aed03d0cd5aacd85c1f31c662bf96149ce5dbc38
Григорьева Марина <mgrigoreva@phoenixit.ru>
2022-09-01 01:07:54 +0300

    cookie

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -115,6 +115,7 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
         });
+      res.cookie('Domain', 'paw.patrol.nomoredomains.sbs');
       res.send({ token });
     })
     .catch(next);
